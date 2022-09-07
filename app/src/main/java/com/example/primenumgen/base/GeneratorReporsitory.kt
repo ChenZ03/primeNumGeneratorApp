9474743d8528df11c62f78ce7ae0aabc935ad81b
Xectroz <choochenzhung@gmail.com>
2022-09-07 17:31:11 +0800

    prime number algo

--- app/src/main/java/com/example/primenumgen/base/GeneratorReporsitory.kt
+++ app/src/main/java/com/example/primenumgen/base/GeneratorReporsitory.kt
@@ -2,8 +2,23 @@ package com.example.primenumgen.base
 
 class GeneratorReporsitory {
 
-    fun getPrimeNumbers(value: Int, value1: Int): Any {
-        TODO("Not yet implemented")
+   fun getPrimeNumbers(startNum : Int , endNum : Int): List<Int> {
+       val primeNumbers = mutableListOf<Int>()
+       for (i in startNum..endNum) {
+           if (isPrime(i)) {
+               primeNumbers.add(i)
+           }
+       }
+       return primeNumbers
+    }
+
+    private fun isPrime(number: Int): Boolean {
+        for (i in 2 until number) {
+            if (number % i == 0) {
+                return false
+            }
+        }
+        return true
     }
 
     companion object{

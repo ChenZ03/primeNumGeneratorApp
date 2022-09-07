45d43f2538b765538e595207f4423a2a20959ff5
Xectroz <choochenzhung@gmail.com>
2022-09-07 22:06:38 +0800

    #5 divide to coroutines

--- app/src/main/java/com/example/primenumgen/Generator/GeneratedViewModel.kt
+++ app/src/main/java/com/example/primenumgen/Generator/GeneratedViewModel.kt
@@ -2,6 +2,10 @@ package com.example.primenumgen.Generator
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.*
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlin.math.floor
 
 
 class GeneratedViewModel() : ViewModel() {
@@ -17,11 +21,43 @@ class GeneratedViewModel() : ViewModel() {
 
     suspend fun generatePrimeNums(startNum: Int, endNum: Int): MutableList<Int> {
         val primeNumbers = mutableListOf<Int>()
-        for (i in startNum..endNum) {
-            if (isPrime(i)) {
-                primeNumbers.add(i)
+        val allNum = (startNum..endNum).toList()
+        val lock = Mutex()
+        if(endNum - startNum > 10000){
+            // divide to coroutines
+            val numOfCoroutines = floor((endNum - startNum) / 10000.0).toInt()
+            coroutineScope {
+                async(Dispatchers.Default) {
+                    for (i in numOfCoroutines * 1000 until allNum.size) {
+                        if (isPrime(allNum[i])) {
+                            lock.withLock {
+                                primeNumbers.add(allNum[i])
+                            }
+                        }
+                    }
+                }
+                for (i in 1..numOfCoroutines) {
+                    for (n in i * 1000 - 1000..i * 1000) {
+                        async(Dispatchers.Default) {
+                            if (isPrime(allNum[n])) {
+                                lock.withLock {
+                                    primeNumbers.add(allNum[n])
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }else{
+            for (i in startNum until endNum) {
+                if (isPrime(i)) {
+                    primeNumbers.add(i)
+                }
             }
         }
+
+        primeNumbers.sort()
         return primeNumbers
     }
 

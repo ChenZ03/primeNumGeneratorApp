191e9255f0795917af0dd778ea2896b937d50e4b
Xectroz <choochenzhung@gmail.com>
2022-09-07 19:20:04 +0800

    #9 navigation between fragment

--- app/src/main/java/com/example/primenumgen/Generator/GeneratedFragment.kt
+++ app/src/main/java/com/example/primenumgen/Generator/GeneratedFragment.kt
@@ -5,22 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
 import com.example.primenumgen.R
+import com.example.primenumgen.databinding.FragmentGeneratedBinding
 
 
 class GeneratedFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    lateinit var binding: FragmentGeneratedBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_generated, container, false)
+    ): View {
+        binding = FragmentGeneratedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val args = arguments
+        binding.back.setOnClickListener {
+            NavHostFragment.findNavController(this).popBackStack()
+        }
+
     }
 
 }
--- app/src/main/java/com/example/primenumgen/home/homeFragment.kt
+++ app/src/main/java/com/example/primenumgen/home/homeFragment.kt
@@ -5,22 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.primenumgen.R
+import com.example.primenumgen.databinding.FragmentHomeBinding
+import androidx.navigation.fragment.NavHostFragment
 
 
 class homeFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    lateinit var binding : FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        navigate to next screen
+        binding.generate.setOnClickListener {
+            val action = homeFragmentDirections.actionHomeFragmentToGeneratedFragment(1,2)
+            NavHostFragment.findNavController(this).navigate(action)
+        }
     }
 }
